c8ceb43039e301906e5074a69000ebffafeb1757
xu1jia2qi3 <jiaqy@live.com>
2020-02-18 14:44:40 -0500

    delete old

--- config/prod.env.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  NODE_ENV: '"production"',
-  BASE_API: '"https://127.0.0.1:4567"'
-}
--- src/utils/firestore.js
+++
@@ -1,9 +0,0 @@
-import firebase from 'firebase'
-import 'firebase/firestore'
-
-const db = firebase.firestore()
-const settings = {
-  timestampsInSnapshots: true
-}
-db.settings(settings)
-export default db
